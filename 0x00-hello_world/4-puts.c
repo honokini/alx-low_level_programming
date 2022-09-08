69b8ee8e7d9168401461a0958f0d58589c65013c
honokini <hkini48@gmail.com>
2022-09-08 14:54:15 -0700

    My third commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+/**
+* main -print the string in the put function
+* Description: use the main function
+* this programme prints "Programming is like building a multilingual puzzle
+* return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
