a8142eb5f28c4baa94aa33314cd71127e1787e5d
honokini <hkini48@gmail.com>
2022-09-08 15:26:37 -0700

    My fourth commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /**
 * main - print the string in the put function
 * Description: using the main function
-* this programme prints "Programming is like building a multilingual puzzle
+* this program prints "Programming is like building a multilingual puzzle
 * return: 0
 */
 int main(void)
