fa1d4a6accc5e40197f97b566fa0f00ec712518e
honokini <hkini48@gmail.com>
2022-09-12 18:25:52 -0700

    My second commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 /**
@@ -11,9 +10,9 @@
 int main(void)
 { 
 char ch;
-for ( ch = 'a'; ch <= 'z'; ch++ )
+for (ch = 'a';ch <= 'z'; ch++)
 {
-     putchar(ch);
+putchar(ch);
 }
 putchar('\n');
 return (0);
