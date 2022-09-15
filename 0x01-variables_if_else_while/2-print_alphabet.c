baa436c7f6e2637bdb76b0e409ee7fc2196ea1ed
honokini <hkini48@gmail.com>
2022-09-15 17:10:13 +0000

    Fixed file2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,11 +3,11 @@
 #include <time.h>
 #include <stdio.h>
 /**
-* main - print if the number is positive, zero, or negative
+*main - print if the number is positive, zero, or negative
 *
-* Description: using the main function
-* this program prints "Programmin is positive,zero, or negative"
-* Return: 0
+*Description: using the main function
+*this program prints "Programmin is positive,zero, or negative"
+*Return: 0
 */
 int main(void)
 { 
