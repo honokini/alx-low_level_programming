75ab0ef6f5ad90a4364f49b62bf52e182ef41023
honokini <hkini48@gmail.com>
2022-09-15 16:55:11 +0000

    Fixed file4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,11 +3,11 @@
 #include <stdlib.h>
 
 /**
-* main - print if the number is positive, zero, or negative 
+*main - print if the number is positive, zero, or negative 
 * 
-* Description: using the main function
-* this program prints "prgramming is positive,zero, or negative
-* Return: 0
+*Description: using the main function
+*this program prints "prgramming is positive,zero, or negative
+*Return: 0
 */
 int main(void)
 {
