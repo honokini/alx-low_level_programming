6f102c3a619a6121eeff176f97a91b70d367c4a7
honokini <hkini48@gmail.com>
2022-09-15 20:11:17 +0000

    File102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -23,18 +23,18 @@ putchar((n1 / 10) + 48);
 putchar((n1 % 10) + 48);
 putchar(' ');
 putchar((n2 / 10) + 48);
-putchar((n2 % 10) + 48);
+putchar((n1 % 10) + 48);
 
 if (n1 != 98 || n2 != 98)
 {
-putchar(' , ');
+putchar(',');
 putchar(' ');
 
 }
 }
 ++n2;
 }
-++n1
+++n1;
 }
 putchar('\n');
 return 0;
