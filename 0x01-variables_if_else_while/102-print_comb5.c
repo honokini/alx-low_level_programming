082c4c01fe59292cce59703550e4bd2782ac9d8b
honokini <hkini48@gmail.com>
2022-09-15 20:08:01 +0000

    File102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <time.h>
+#include <stdio.h>
+/**
+*
+*
+*Description: using the main function
+*
+*
+*
+*
+*/
+int main()
+{
+int n1 = 0, n2;
+while (n1 <= 99)
+{n2 = n1;
+
+while(n2 <= 99)
+{
+if (n2 != n1)
+{
+putchar((n1 / 10) + 48);
+putchar((n1 % 10) + 48);
+putchar(' ');
+putchar((n2 / 10) + 48);
+putchar((n2 % 10) + 48);
+
+if (n1 != 98 || n2 != 98)
+{
+putchar(' , ');
+putchar(' ');
+
+}
+}
+++n2;
+}
+++n1
+}
+putchar('\n');
+return 0;
+}
