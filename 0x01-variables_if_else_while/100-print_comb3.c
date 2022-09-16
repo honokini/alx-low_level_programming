b9f98f6e720477d9b4eb32257b9fb086b6736157
honokini <hkini48@gmail.com>
2022-09-16 14:07:42 +0000

    Ok

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,10 +18,17 @@ int main()
 				putchar(i + '0');
                                 putchar(j + '0');
                                
+				if(i != 8)
+				{
+					putchar(',');
+					putchar(' ');
 
 			}
 
 		}
+	}
+	}
+
 	
 
 putchar('\n');
