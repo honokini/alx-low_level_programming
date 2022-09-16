7e44a749e517c75c438b32574ec930e0d91f8be2
honokini <hkini48@gmail.com>
2022-09-16 13:37:03 +0000

    Fixed task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,21 +7,20 @@
 */
 int main()
 {
-	int i;
-	int j;
+	int i,j;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 1; j <=9; j++)
 		{
-			if (j>i)
+			if (j > i)
 			{
 				putchar(i + '0');
 				putchar(j + '0');
 			}
 		}
 	}
-}
+
 putchar('\n');
 
 return (0);
