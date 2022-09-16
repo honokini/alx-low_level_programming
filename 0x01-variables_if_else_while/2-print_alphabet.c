d3d8078d7bcde30972efc7aaa9eac7661b274055
honokini <hkini48@gmail.com>
2022-09-16 11:28:31 +0000

    Fixed file2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,17 +2,19 @@
 #include <time.h>
 #include <stdio.h>
 /**
-*main - lowercase alphabets
+* main - lowercase alphabets
 *
-*Return:  Always(Success)
+* Return:  Always(Success)
 */
 int main(void)
 { 
 char ch;
+
 for (ch = 'a'; ch <= 'z'; ch++)
 {
 putchar(ch);
 }
+
 putchar('\n');
 return (0);
 }
