315964b767e03f63fda1510010c063a125cfae9f
honokini <hkini48@gmail.com>
2022-09-16 20:29:20 +0000

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
+int main(Void)
+{
+	int i;
+	
+	i = 0;
+	postive_or_negative(i);
+
+	return (0);
+}
