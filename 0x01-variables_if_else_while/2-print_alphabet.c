a6d2239a9164cf64fee9e994bc539b3effab9f4a
honokini <hkini48@gmail.com>
2022-09-16 12:41:46 +0000

    File2 ok

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,14 +8,15 @@
 */
 int main(void)
 { 
-char c;
+	char c;
 
-for (c = 'a'; c <= 'z'; c++)
-{
-putchar(c);
-}
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
 
-putchar('\n');
-return (0);
+	putchar('\n');
+
+	return (0);
 
 }
