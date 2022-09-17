6cf72c0d68eab8825d5b44c86f18942ea76c5dd8
honokini <hkini48@gmail.com>
2022-09-17 18:43:58 +0000

    File8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,9 +13,10 @@ void jack_bauer(void)
 	int minutes_remainder;
 
 	while (hours <= 23)
-	{
+	{ 
 		while (minutes <= 59)
 		{
+			minutes_remainder = minutes % 10;
 			hours_remainder = hours % 10;
 			_putchar(hours / 10 + '0');
 			_putchar(hours_remainder + '0');
