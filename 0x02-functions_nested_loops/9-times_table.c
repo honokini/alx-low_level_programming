5a3d57f16126e7f70dada65dd936eeb991321460
honokini <hkini48@gmail.com>
2022-09-17 19:21:10 +0000

    Fixed task9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,8 +23,8 @@ void times_tables(void)
 		
 		else
 		
-			_putchar(prod / 10 + '0');
-		        _putchar((prod % 10) + '0');
+			_putchar((prod / 10 + '0');
+					_putchar((prod % 10) + '0');
 		}
 	         _putchar('\n');
 	}
