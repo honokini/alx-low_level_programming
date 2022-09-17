b1f0a0805ca420aef38e0b4bb80dbc857b8f075d
honokini <hkini48@gmail.com>
2022-09-17 19:48:28 +0000

    My last tasks

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,13 +18,17 @@ void times_tables(void)
 		        prod = num * mult;
 
 		        if(prod <= 9)
+			{
 		
 			        _putchar(' ');
+			}
 		
 		else
+		{
 		
 			        _putchar((prod / 10 + '0');
-		_putchar((prod % 10) + '0');
+		                _putchar((prod % 10) + '0');
+		}
 	}
 	 _putchar('\n');
    }
