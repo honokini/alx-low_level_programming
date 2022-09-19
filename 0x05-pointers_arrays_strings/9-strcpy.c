7178bb282a370a6555bcc2a6e9ae4adc1aa4249d
honokini <hkini48@gmail.com>
2022-09-19 18:35:35 +0000

    Task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcpy - copiar el contenido de uno a otra variable
+ * @dest: This is destiny
+ * @str: This is the copia
+ *
+ * Return: This returns copy
+ */
+char *_strcp(char *dest, char *str)
+{
+	char *start = dest;
+
+	while (*scr != '\0')
+	{
+		*dest = *scr;
+		dest++;
+		scr++;
+	}
+	*destv= '\0';
+	return (start);
+}
