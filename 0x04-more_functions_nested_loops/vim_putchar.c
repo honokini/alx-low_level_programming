3dfa5234f9e99a597e56d570a3a698da48f6b847
honokini <hkini48@gmail.com>
2022-09-20 02:34:50 +0000

    Ok for my task file1

---
+++ 0x04-more_functions_nested_loops/vim_putchar.c
@@ -0,0 +1,13 @@
+#include<unistd.h>
+#include "main.h"
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On sucess 1.
+ * On erro. -1 is returned and erno is set appropriately
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
