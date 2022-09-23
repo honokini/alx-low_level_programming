383c93f7eab1de0c3ff60d8284fa2fe5e1507604
honokini <hkini48@gmail.com>
2022-09-23 22:56:34 +0000

    Task 1-isdigit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,13 +1,23 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File - main.h
- * Author: Genius Villagers
- * Desc: Header file contains prototypes for all functions
- *       used in 0x03-more_functions_nested_loops directory
- */
+#include <stdio.h>
+
+
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_number(int n);
 int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+
+#endif
+
 
 
 
