a5ea749643832a7a118dbcf08b50275523596909
honokini <hkini48@gmail.com>
2022-09-23 02:09:24 +0000

    Task 1 is done

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,17 +11,13 @@ int main(void)
 
     char s1[98] = "Hello ";
     char s2[] = "World!\n";
-    char *ptr;
+    char *p;
 
     printf("%s\n", s1);
     printf("%s", s2);
-    ptr = _strncat(s1, s2);
-    printf("%s\n", s1);
-    printf("%s", s2);
-    printf("%s\n", ptr);
-    ptr = _strncat(s1, s2);
+    p = _strncat(s1, s2);
     printf("%s", s1);
     printf("%s", s2);
-    printf("%s", ptr);
+    printf("%s", p);
     return (0);
 }
