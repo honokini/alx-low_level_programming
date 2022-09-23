47e5dfb05f1f807f47fd545fc8b91ff246717513
honokini <hkini48@gmail.com>
2022-09-23 02:33:35 +0000

    Task 01

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,7 +3,8 @@
 /**
  * _putchar - concatenates two strings using at most
  *           an inputted number of bytes from src.
- * @dest: The string to be appended to dest.
+ * @dest: The string to be appended upon. 
+ * @src: The string to be appended to dest.
  * @n: The number of bytes from src to be appended to dest
  *
  * Return: A pointer to the resulting string dest.
