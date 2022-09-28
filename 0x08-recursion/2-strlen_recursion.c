e110ee53d016c7d4d1f8991e5ebe75c82c93078b
honokini <hkini48@gmail.com>
2022-09-28 04:46:30 +0000

    We have printed task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,11 +5,12 @@
  * Return strln_recursion
  */
 int _strlen_recursion(char *s)
-	{
+{
 		/*Base condition*/
 		if (*s == '\0')
-		return0;
+		return 0;
 
 		else
 			return (1 + _strlen_recursion(s + 1)); /*Sum 1*/
-	}
+}
+
