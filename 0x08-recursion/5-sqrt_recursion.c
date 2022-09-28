13e8f2f76d1f03457ee0fceaace8500dd80463c5
honokini <hkini48@gmail.com>
2022-09-28 06:09:19 +0000

    We have 5-sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _evaluate -evaluate function sqrt
+ * @i: integer
+ * @n: integer
+ * Return: evaluate sqrt
+ */
+int _evaluate(int i, int n)
+{
+	/*Evaluate function*/
+	if (n == 0 || n == 1)
+		return (n);
+	else if (i * i < n)
+		return (i);
+	return (- 1);
+	return (- 1);
+}
