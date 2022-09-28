96a8df1422810a03b6bc30aff54e9a4aeaf4b1d0
honokini <hkini48@gmail.com>
2022-09-28 23:43:14 +0000

    Fixed 0x08-recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,19 +2,17 @@
 /**
  * _pow_recursion - returns the value of x raised to the power of y
  * @x: integer
- * @y: integer
+ * @y: interger
  * Return: pow recursion
  */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0) /*Base condition*/
 	{
-		return (- 1);
+		return (-1);
 	}
 	else if (y != 0)
-		return (x * _pow_recursion(x, y - 1)); /*Recursion call*/
-	
+		return (x * _pow_recursion(x, y - 1)); /*Recursive call*/
 	else
 		return (1);
-
 }
