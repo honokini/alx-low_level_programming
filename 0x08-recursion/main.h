9ee5e5d6436976f9bdf3a0c8b710c4d48b5674d3
honokini <hkini48@gmail.com>
2022-09-28 04:08:43 +0000

    We have printed a string

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,9 @@
+int _putchar(charc);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int n);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
