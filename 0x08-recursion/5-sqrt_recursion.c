ca4796e70e34ea7750d8d6a33a26a08900e1c6cd
honokini <hkini48@gmail.com>
2022-09-29 03:31:01 +0000

    Task 5-sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -29,7 +29,9 @@ int _sqrt_recursion(int n)
 	int i = 0;
 
         if (i < 0)/*If n is negative*/
+		
 		return (-1);
+
         else
         {
                 return (_evaluate(i, n)); /*Recursive call*/
