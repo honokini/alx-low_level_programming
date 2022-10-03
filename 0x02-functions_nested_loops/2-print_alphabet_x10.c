b9f7d5950af6281aa293a89482cb9d759ef02ea0
honokini <hkini48@gmail.com>
2022-10-02 17:54:03 -0700

    Fixed 2-print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,17 +4,16 @@
  *
  * Return: 0
  */
-void print_alphabet_x10(void) 
+void print_alphabet_x10(void)
 {
 	int j, x;
 
 	for (x = 0; x <= 9; x++)
 	{
 		for (j = 'a'; j <= 'z'; j++)
-		{
-			_putchar(j);
-		}
+			{
+				_putchar(j);
+			}
 		_putchar('\n');
-
 	}
 }
