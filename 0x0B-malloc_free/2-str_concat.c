e196edd74442fd3ac6da2cb77cfe867aa419307d
honokini <hkini48@gmail.com>
2022-10-05 08:54:26 -0700

    Task 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,23 +1,23 @@
-#include "main.h"                                                                                                                                                        
-/**                                                                                                                                                                      
- *  * str_concat  - a function that concatenates two strings.                                                                                                               
- *   * @s1: First string                                                                                                                                                     
- *    * @s2: Second string                                                                                                                                                    
- *     *                                                                                                                                                                       
- *      * Return: (NULL) in case of failure, but pointer to a new string                                                                                                        
- *       * in case of success                                                                                                                                                    
- *        */                                                                                                                                                                      
-char *str_concat(char *s1, char *s2)                                                                                                                                     
-{                                                                                                                                                                        
-	        char *concat_str ;                                                                                                                                                
-		        int index, concat_index = 0,  len = 0;                                                                                                                           
-			                                                                                                                                                                         
-			        if (s1 == NULL)                                                                                                                                                  
-					                s1 = "";                                                                                                                                                 
-				                                                                                                                                                                         
-				        if (s2 == NULL)                                                                                                                                                  
-						                s2 = "";                                                                                                                                                 
-					                                                                                                                                                                         
-					        for (index = 0; s1[index] || s2[index]; index++)                                                                                                                 
-							                concat_str = malloc(sizeof(char) * len); 
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	    char *s;
+
+	    s = str_concat("Betty ", "Holberton");
+		    if (s == NULL)
+		    {
+			    printf("failed\n");
+			    return (1);
+
+			    printf("%s\n", s);
+			    free(s);
+			    return (0);
+}
