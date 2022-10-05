8a635c0cf226946ef1d841f1bf9372333428aae7
honokini <hkini48@gmail.com>
2022-10-04 22:32:31 -0700

    Commit 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,50 +1,23 @@
-#include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
-/**
- * str_concat - concatenates 2 strings.
- * a NULL string is treated  as an empty string.
- * @s1: pointer to string.
- * @s2: pointer to string.
- *
- * Return: pointer to newly allocated memory which s1, s2 and NULL byte.
- * NULL on failure.
- */
-char *str_concat(char *s1, char *s2)
-{
-	unsigned int len1, len2, size, i, j;
-	char *nstr;
+#include "main.h"                                                                                                                                                        
+/**                                                                                                                                                                      
+ *  * str_concat  - a function that concatenates two strings.                                                                                                               
+ *   * @s1: First string                                                                                                                                                     
+ *    * @s2: Second string                                                                                                                                                    
+ *     *                                                                                                                                                                       
+ *      * Return: (NULL) in case of failure, but pointer to a new string                                                                                                        
+ *       * in case of success                                                                                                                                                    
+ *        */                                                                                                                                                                      
+char *str_concat(char *s1, char *s2)                                                                                                                                     
+{                                                                                                                                                                        
+	        char *concat_str ;                                                                                                                                                
+		        int index, concat_index = 0,  len = 0;                                                                                                                           
+			                                                                                                                                                                         
+			        if (s1 == NULL)                                                                                                                                                  
+					                s1 = "";                                                                                                                                                 
+				                                                                                                                                                                         
+				        if (s2 == NULL)                                                                                                                                                  
+						                s2 = "";                                                                                                                                                 
+					                                                                                                                                                                         
+					        for (index = 0; s1[index] || s2[index]; index++)                                                                                                                 
+							                concat_str = malloc(sizeof(char) * len); 
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	len1 = 0;
-	while (s1[len1] != '\0')
-		len1++;
-	len2 = 0;
-	while (s2[len2] != '\0')
-		len2++;
-
-	size = len1 + len2;
-
-	nstr = malloc((sizeof(char) * size) + 1);
-	/*check if malloc was successful*/
-	if (nstr = NULL)
-		return (NULL);
-
-	i = 0;
-	while (i < len1)
-	{
-		nstr[i] = s1[i];
-		i++;
-	}
-	j = 0;
-	while (i <= size)
-	{
-		nstr[i] = s2[j];
-		i++;
-		j++;
-	}
-	return (nstr);
-}
