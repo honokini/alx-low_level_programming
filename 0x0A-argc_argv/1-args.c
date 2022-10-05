f19490145ae12e0b853bc34d9321a0548c4678e4
honokini <hkini48@gmail.com>
2022-10-05 16:11:24 -0700

    Commit 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,6 +2,7 @@
 
 /**
  * main - prints the name number of arguments passed to it
+ * @argc: number of arguments passed to the function
  * @argv: argument vector of pointers to strings
  *
  * Return: always 0
