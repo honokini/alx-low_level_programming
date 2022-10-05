04f079a8e2927af021824fef01fcc4c86d54476d
honokini <hkini48@gmail.com>
2022-10-04 21:30:47 -0700

    Commit 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,7 +9,7 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *concat_str;
+	char *str_concat;
 	int index, concat_index = 0,  len = 0;
 
 	if (s1 == NULL)
@@ -19,16 +19,16 @@ char *str_concat(char *s1, char *s2)
 		s2 = "";
 
 	for (index = 0; s1[index] || s2[index]; index++)
-		concat_str = malloc(sizeof(char) * len);
+		str_concat = malloc(sizeof(char) * len);
 
-	if (concat_str == NULL)
+	if (str_concat == NULL)
 		return (NULL);
 
 	for (index = 0; s1[index]; index++)
-		concat_str[concat_index++] = s1[index];
+		str_concat[concat_index++] = s1[index];
 
 	for (index = 0; s2[index]; index++)
-		concat_str[concat_index++] = s2[index];
+		str_concat[concat_index++] = s2[index];
 
-	return (concat_str);
+	return (str_concat);
 }
