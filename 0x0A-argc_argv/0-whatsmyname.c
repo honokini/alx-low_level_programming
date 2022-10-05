f8eda50c0833e435dbadb3f8b74e153f0c2b6b12
honokini <hkini48@gmail.com>
2022-10-05 15:56:23 -0700

    Commit 0-whatsname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,8 +5,8 @@
  * @argv: argument vector of pointers to string
  * Return: always 0
  */
-int main(int argc, char __attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", argc[0]);
-	return 0;
+	printf("%s\n", argv[0]);
+	return (0);
 }
