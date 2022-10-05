e720311c9310911348174fe04f38e20e363434c9
honokini <hkini48@gmail.com>
2022-10-05 10:37:15 -0700

    Task 2-str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,23 +1,40 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
- * main - check the code for ALX School students.
- *
- * Return: Always 0.
+ * str_concat -> concatinating function
+ * @s1: string 1
+ * @s2: string 2
+ * Return: string 1 + string 2
  */
-int main(void)
+char *str_concat(char *s1, char *s2)
 {
-	    char *s;
+	int i = 0, j = 0, l = 0, k = 0;
+	char *s;
 
-	    s = str_concat("Betty ", "Holberton");
-		    if (s == NULL)
-		    {
-			    printf("failed\n");
-			    return (1);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[i])
+		i++;
+	while (s2[j])
+		j++;
 
-			    printf("%s\n", s);
-			    free(s);
-			    return (0);
+	l = i + j;
+	s = (char *) malloc(l * sizeof(char) + 1);
+	if (s == NULL)
+		return (NULL);
+	j = 0;
+	while (k < l)
+	{
+		if (k < i)
+			s[k] = s1[k];
+		if (k >= i)
+		{
+			s[k] = s2[j];
+			j++;
+		}
+		k++;
+	}
+	s[k] = '\0';
+	return (s);
 }
