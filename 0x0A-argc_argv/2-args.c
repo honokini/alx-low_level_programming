5ab44a641e7dbfea56e4488e4881c11c9fc72300
honokini <hkini48@gmail.com>
2022-10-05 18:11:39 -0700

    Commit my important task

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,7 +7,7 @@
  *
  * Return: always 0
  */
-int main(int argc, int *argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,15 +10,15 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, mul;
+	int num_1, num_2, mul;
 
-	if (arg != 3)
+	if (argc != 3)
 		printf("Error\n");
 	else
 	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[2]);
-		mul = num1 * num2;
+		num_1 = atoi(argv[1]);
+		num_2 = atoi(argv[2]);
+		mul = num_1 * num_2;
 		printf("%d\n", mul);
 	}
 	return (0);
