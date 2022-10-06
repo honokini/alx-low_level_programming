bd1c1c39dcae367899c5d41a713589c1085d685d
honokini <hkini48@gmail.com>
2022-10-06 09:40:57 -0700

    Task 1-string_nconcatc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,13 +15,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *concat;
 	unsigned int len = n, index;
 
-	if (S1 == NULL)
+	if (s1 == NULL)
 		s1 ="";
+
 	if (s2 == NULL)
 		s2 = "";
 
 	for (index = 0; s1[index]; index++)
 		len++;
+
 	concat = malloc(sizeof(char) * (len + 1));
 
 	if (concat == NULL)
@@ -34,6 +36,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (index = 0; s2[index] && index < n; index++)
 		concat[len++] = s2[index];
+
 	concat[len] = '\0';
+
 	return (concat);
 }
