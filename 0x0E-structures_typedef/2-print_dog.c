f466ba9ec36ff628a56edcf1915129fe0a2c09c3
honokini <hkini48@gmail.com>
2022-10-10 09:07:27 -0700

    Commit 2-print-dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,7 +8,7 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d ==NULL)
+	if (d == NULL)
 		return;
 
 	if (d->name == NULL)
@@ -22,7 +22,7 @@ void print_dog(struct dog *d)
 		printf("age: %f\n", d->age);
 
 
-	if (d->owner ==NULL)
+	if (d->owner == NULL)
 		printf("owner: (nil)\n");
 	else
 		printf("owner: %s\n", d->owner);
