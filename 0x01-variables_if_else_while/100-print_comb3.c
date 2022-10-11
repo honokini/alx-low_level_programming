5ab7d15e38e8366d84dba086fc871f63dcf1d43f
honokini <hkini48@gmail.com>
2022-10-10 22:11:27 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,10 +10,10 @@ int main(void)
 {
 	int d;
 
-	for (d = 1; d < 90; d++)
+	for (d = 1; d <= 89; d++)
 	{
-		putchar((d / 11) + '0');
-		putchar((d % 11) + '0');
+		putchar((d / 10) + '0');
+		putchar((d % 10) + '0');
 		if (d != 89)
 		{
 			putchar(',');
