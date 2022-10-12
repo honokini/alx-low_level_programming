2b65ea2c130e001c4c12e20670ec4dab56737c68
honokini <hkini48@gmail.com>
2022-10-12 14:02:50 -0700

    Terminal task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include "3-calc.h"
+/**
+ * main - Check arguments
+ * @argc: argument count
+ * @argv: argument vector.
+ *
+ * Return: Error if number of arguments is wrong
+ */
+int main(int argc, char *argv[])
+{
+	int a = 0, b = 0, res = o;
+	char s;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	/*There is only one operator*/
+	if (argv[2][1] != '\n')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	s = argv[2][0];
+	if (s != '+' && s != '-' && s != '/' && s != '*' && s != '%')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	res = (get_op_func(argv[2]))(a, b);
+	printf("%d\n", res);
+	return (0);
+}
