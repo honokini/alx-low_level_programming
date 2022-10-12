09309803f8e6d6bebdaa694f4ec3b04771f6317e
honokini <hkini48@gmail.com>
2022-10-11 20:47:58 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+/**
+ * print_name - Prints a name.
+ * @namee: Pointer to name.
+ * @f: function pointer.
+ *
+ * Return: Void.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
