be773c1b382e2201502c9488217e47b14216d5b8
honokini <hkini48@gmail.com>
2022-10-31 04:13:03 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -20,25 +20,24 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
-		fd = open(filename, O_RDONLY);
-		if (fd == -1)
-			return (0);
-		buffer = malloc(sizeof(char) * letters);
-		if (buffer == NULL)
-		{
-			close(fd);
-			return (0);
-		}
-		lenr = read(fd, buffer, letters);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
 		close(fd);
-		if (lenr == -1)
-		{
-			free(buffer);
-			return (0);
-		}
-		lenw = write(STDOUT_FILENO, buffer, lenr);
+		return (0);
+	}
+	lenr = read(fd, buffer, letters);
+	close(fd);
+	if (lenr == -1)
+	{
 		free(buffer);
-		if (lenr != lenw)
-			return (0);
-		return (lenw);
+		return (0);
+	}lenw = write(STDOUT_FILENO, buffer, lenr);
+	free(buffer);
+	if (lenr != lenw)
+		return (0);
+	return (lenw);
 }
