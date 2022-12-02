be092f5e9042e2438cf856417bcef4d421b83274
honokini <hkini48@gmail.com>
2022-12-02 09:03:27 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,23 +1,20 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * more_numbers - prints numbers to 14
  *
- * Return: returns nothing
+ * Return: no return
  */
 void more_numbers(void)
 {
-	int n1, n2;
-	for (n1 = 0; n1 < 10; n++)
-	{
-		for (n2 = 0; n2 <= 14; n2++)
-		{
-			if (n2 > 9)
-			{
-				putchar((n2 / 10) + '0');
-			}
-			putchar((n2 % 10) + '0');
-		}
-		putchar(10);
-	}
+int i, j;
+for (i = 1; i <= 10; i++)
+{
+for (j = 0; j < 15; j++)
+{
+if (j >= 10)
+_putchar('1');
+_putchar(j % 10 + '0');
+}
+_putchar('\n');
+}
 }
