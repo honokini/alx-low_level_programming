374d2dd6b9d8d5b1f3ac33c84b2674e795e04798
honokini <hkini48@gmail.com>
2022-12-18 20:39:45 -0800

    0-read_textfile.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,7 +9,7 @@
 #include <unistd.h>
 
 int _putchar(char c);
-ssize_t read_texfile(const char *filename, size_t letters);
+ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int _strln(char *str);
 int append_text_to_file(const char *filename, char *text_content);
