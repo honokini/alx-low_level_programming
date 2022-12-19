9af0d94e2f5ba5611dfd896c3bf8080ba8a849fb
honokini <hkini48@gmail.com>
2022-12-18 21:48:44 -0800

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,58 +1,45 @@
-#include  "main.h"
+#include "main.h"
 #include <stdlib.h>
 
 /**
- * _realloc - Reallocates a memory block  using  malloc and free.
- * @ptr:  A pointer to the memory previously allocated.
- * @old_size: The size in bytes of allocated space for ptr.
- * @new_size: The size in bytes for the new memory block.
+ * _realloc - reallocates a memory block  using  malloc and free.
+ * @ptr: pointer to the memory previously allocated by malloc.
+ * @old_size: size of the allocated space for ptr.
+ * @new_size: new size of the new memory block.
  *
- * Return: If new_size == old_size - ptr
- *     If new_size == 0 and ptr is not NULL - NULL
- *     Otherwise - a pointer to the reallocated memory block.
+ * Return: pointer to the newly allocated memory block.
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *mem;
-	char *ptr_copy, *filler;
-	unsigned int index;
+	char *ptr1;
+	char *old_ptr;
+	unsigned int i;
 
 	if (new_size == old_size)
 		return (ptr);
 
-	if (ptr == NULL)
-	{
-		mem = malloc(new_size);
-
-		if (mem == NULL)
-			return (NULL);
-
-		return (mem);
-	}
-	if (new_size == 0 && ptr != NULL)
-	{
-		(mem);
-	}
-	{
-
-	free(ptr);
-	return (NULL);
-	}
-
-	ptr_copy = ptr;
-	mem = malloc(sizeof(*ptr_copy) * new_size);
-
-	if (mem == NULL)
+	if (new_size == 0 && ptr)
 	{
 		free(ptr);
 		return (NULL);
 	}
 
-	filler = mem;
+	if (!ptr)
+		return (malloc(new_size));
 
-	for (index = 0; index < old_size && index < new_size; index++)
-		filler[index] = *ptr_copy++;
+	ptr1 = malloc(new_size);
+
+	if (!ptr1)
+		return (NULL);
+
+	old_ptr = ptr;
+
+	if (new_size < old_size)
+	{
+		for (i = 0; i < old_size; i++)
+			ptr1[i] = old_ptr[i];
+	}
 
 	free(ptr);
-	return (mem);
+	return (ptr1);
 }
