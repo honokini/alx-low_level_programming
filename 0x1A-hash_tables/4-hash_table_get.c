8ec22287d9c69a0420b3df6bdf37a062344a23b4
honokini <hkini48@gmail.com>
2023-01-09 13:16:31 -0800

    4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -6,8 +6,8 @@
  * @ht: A pointer to the hash table.
  * @key: The key to get the value of.
  *
- * Return: If the key cannot be matched -NULL.
- *         Otherwise - the value associated with the key in ht.
+ * Return: If the key cannot be matched - NULL.
+ *         Otherwise - the value associated with key in ht.
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
@@ -17,7 +17,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
 
-	index = key_index((const char *)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 	if (index >= ht->size)
 		return (NULL);
 
@@ -25,5 +25,5 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	while (node && strcmp(node->key, key) != 0)
 		node = node->next;
 
-	return ((node = NULL) ? NULL : node->value);
+	return ((node == NULL) ? NULL : node->value);
 }
